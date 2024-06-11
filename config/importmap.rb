d3e8a9085cb264d394cf534cde1358635ea6f7e6
Boban Jovanoski <boban.jovanoski@xing.com>
2024-06-11 19:34:42 +0200

    add bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-# Pin npm packages by running ./bin/importmap
-
 pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin 'jquery', preload: true # @3.7.1
+pin 'bootstrap', preload: true # @5.3.3
+pin '@popperjs/core', to: '@popperjs--core.js', preload: true # @2.11.8
 pin_all_from 'app/javascript/controllers', under: 'controllers'
